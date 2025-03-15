2b25b5ed013ac2798809548b010fec06e1cb3aba
Farhadur Rahman <fr.farhad.ur.rahman@gmail.com>
2025-03-15 15:41:20 +0600

    "Spring boot rest pojo."

---
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/farhad/demo/rest/StudentRestController.java
@@ -0,0 +1,25 @@
+package com.farhad.demo.rest;
+
+import com.farhad.demo.entity.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+    // define endpoint for "/students" - return list of students
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        List<Student> theStudents = new ArrayList<Student>();
+        theStudents.add(new Student("Poornima", "Patel"));
+        theStudents.add(new Student("Mario", "Rossi"));
+        theStudents.add(new Student("Mary", "Smith"));
+
+        return theStudents;
+    }
+}
