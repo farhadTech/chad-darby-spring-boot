ee8298efc9d7c035f23f8e038736c393f46ddbc1
Farhadur Rahman <fr.farhad.ur.rahman@gmail.com>
2025-03-15 22:42:34 +0600

    "refactoring code."

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/farhad/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/farhad/demo/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.farhad.demo.rest;
 
 import com.farhad.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,15 +12,20 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-    // define endpoint for "/students" - return list of students
+    private List<Student> theStudents;
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> theStudents = new ArrayList<Student>();
+    // define @PostConstruct to load the student data
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
         theStudents.add(new Student("Poornima", "Patel"));
         theStudents.add(new Student("Mario", "Rossi"));
         theStudents.add(new Student("Mary", "Smith"));
+    }
 
+    // define endpoint for "/students" - return list of students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return theStudents;
     }
 }
