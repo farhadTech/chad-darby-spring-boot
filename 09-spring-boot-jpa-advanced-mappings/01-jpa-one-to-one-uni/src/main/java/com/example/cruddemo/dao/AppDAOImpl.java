578fd2aab6ce19a02d23f0c366e4476827da9ddf
Farhadur Rahman <fr.farhad.ur.rahman@gmail.com>
2025-03-21 01:31:59 +0600

    AppDao and AppDaoImpl created

---
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/example/cruddemo/dao/AppDAOImpl.java
@@ -0,0 +1,24 @@
+package com.example.cruddemo.dao;
+
+import com.example.cruddemo.entity.Instructor;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+public class AppDAOImpl implements AppDao{
+    // define field for entity manager
+    private EntityManager entityManager;
+
+    // inject entity manager using constructor injection
+    public AppDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Instructor theInstructor) {
+        entityManager.persist(theInstructor);
+    }
+}
+
+
---
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/com/example/cruddemo/dao/AppDao.java
@@ -0,0 +1,7 @@
+package com.example.cruddemo.dao;
+
+import com.example.cruddemo.entity.Instructor;
+
+public interface AppDao {
+    void save(Instructor theInstructor);
+}
