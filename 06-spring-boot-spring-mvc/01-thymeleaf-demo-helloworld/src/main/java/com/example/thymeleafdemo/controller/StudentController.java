97e83c317eb2ce0be25e30872ffc58c7fa31ce64
Farhadur Rahman <fr.farhad.ur.rahman@gmail.com>
2025-03-24 22:56:00 +0600

    Spring MVC form creating the html form and updated controller

--- 06-spring-boot-spring-mvc/01-thymeleaf-demo-helloworld/src/main/java/com/example/thymeleafdemo/controller/StudentController.java
+++ 06-spring-boot-spring-mvc/01-thymeleaf-demo-helloworld/src/main/java/com/example/thymeleafdemo/controller/StudentController.java
@@ -4,6 +4,8 @@ import com.example.thymeleafdemo.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
@@ -19,4 +21,21 @@ public class StudentController {
 
         return "student-form";
     }
+
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student theStudent) {
+
+        // log the input data
+        System.out.println("The student is " + theStudent.getFirstName() + " " + theStudent.getLastName());
+        return "student-confirmation";
+    }
 }
+
+
+
+
+
+
+
+
+
