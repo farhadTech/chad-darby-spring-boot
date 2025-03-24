db502ba4915892f2715ff575434081e9fe1c9bfc
Farhadur Rahman <fr.farhad.ur.rahman@gmail.com>
2025-03-24 16:13:04 +0600

    adding @GetMapping instead of @RequestMapping

--- 06-spring-boot-spring-mvc/01-thymeleaf-demo-helloworld/src/main/java/com/example/thymeleafdemo/controller/HelloWorldController.java
+++ 06-spring-boot-spring-mvc/01-thymeleaf-demo-helloworld/src/main/java/com/example/thymeleafdemo/controller/HelloWorldController.java
@@ -3,13 +3,14 @@ package com.example.thymeleafdemo.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
     // need a controller method to show the initial HTML form
-    @RequestMapping("/showForm")
+    @GetMapping("/showForm")
     public String showForm() {
         return "helloworld-form";
     }
