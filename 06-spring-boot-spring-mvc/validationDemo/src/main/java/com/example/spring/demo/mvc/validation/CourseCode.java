f545c1e1602af2da787dfdbcac13896af8afaa60
Farhadur Rahman <fr.farhad.ur.rahman@gmail.com>
2025-03-25 23:50:55 +0600

    Spring mvc validation: Custom Validation - part 1

---
+++ 06-spring-boot-spring-mvc/validationDemo/src/main/java/com/example/spring/demo/mvc/validation/CourseCode.java
@@ -0,0 +1,19 @@
+package com.example.spring.demo.mvc.validation;
+
+import jakarta.validation.Constraint;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Constraint(validatedBy = CourseCodeConstraintValidaor.class)
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CourseCode {
+    // define default course code
+    public String value() default "LUV";
+
+    // define default error message
+    public String message() default "must start with LUV";
+}
