d4b5f2587d68bd842d1c64e1c8a6b2b29ccd253f
Farhadur Rahman <fr.farhad.ur.rahman@gmail.com>
2025-03-25 17:51:15 +0600

    Spring mvc validation: required field - @InitBinder added

--- 06-spring-boot-spring-mvc/validationDemo/src/main/java/com/example/spring/demo/mvc/CustomerController.java
+++ 06-spring-boot-spring-mvc/validationDemo/src/main/java/com/example/spring/demo/mvc/CustomerController.java
@@ -1,15 +1,27 @@
 package com.example.spring.demo.mvc;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+    // add an initbinder..  to convert trim input strings
+    // remove trailing and leading white space
+    // resolve issue for our validation
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
+
     @GetMapping("/")
     public String showForm(Model theModel) {
         theModel.addAttribute("customer", new Customer());
@@ -20,7 +32,7 @@ public class CustomerController {
     public String processForm(
             @Valid @ModelAttribute("customer")
             Customer theCustomer, BindingResult theBindingResult) {
-
+        System.out.println("last name: " + theCustomer.getLastName());
         if(theBindingResult.hasErrors()) {
             return "customer-form";
         } else {
